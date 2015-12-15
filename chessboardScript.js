e9ec33da91015fb02007743cc7344c844c965b22
anton-kurilov <anton.kurilov@outlook.com>
2015-12-15 12:47:48 -0500

    meeting commit
    added 3rd layer

--- chessboardScript.js
+++ chessboardScript.js
@@ -15,11 +15,11 @@ function drawBoard(canvas, ctx) {
     for (var i = 0; i < 8; i++) {
         for (var j = 0; j < 8; j++) {
             if (!white) {
-                ctx.fillStyle = "rgb(245,222,179)";
+                ctx.fillStyle = "rgb(160,82,45)";
                 ctx.fillRect(j * 75, i * 75, 75, 75);
                 white = true;
             } else {
-                ctx.fillStyle = "rgb(160,82,45)";
+				ctx.fillStyle = "rgb(245,222,179)";
                 ctx.fillRect(j * 75, i * 75, 75, 75);
                 white = false;
             }
@@ -35,20 +35,23 @@ stored
 */
 function pushPieces(ctx) {
     chesspieces.push({
-        c: 9817,
+        unicode: 9817,
+		//colour can always be set to black
         colour: "rgb(0,0,0)",
+		//font can be hardcoded
         font: "70px Arial unicode MS",
         left: 0,
         top: 145,
+		//make width and height static, since each one takes up the square
         width: 75,
         height: 75,
     });
 
     chesspieces.forEach(function(piece) {
-        var c = piece.c;
+        var unicode = piece.unicode;
         ctx.fillStyle = piece.colour;
         ctx.font = piece.font;
-        ctx.fillText(String.fromCharCode(c), piece.left, piece.top);
+        ctx.fillText(String.fromCharCode(unicode), piece.left, piece.top);
     });
 }
 
@@ -58,9 +61,11 @@ This method initializes on load and creates an onclick event.
 function init() {
     canvas = document.getElementById("chessboard");
     canvasGame = document.getElementById("topChessboard");
-    canvasLeft = canvas.offsetLeft,
-        canvasTop = canvas.offsetTop,
-        ctx = canvas.getContext('2d');
+	canvasHighlight = document.getElementById("highlight");
+	
+    canvasLeft = canvas.offsetLeft;
+    canvasTop = canvas.offsetTop;
+    ctx = canvas.getContext('2d');
     ctxGame = canvasGame.getContext('2d');
     drawBoard(canvas, ctx);
     pushPieces(ctxGame);
