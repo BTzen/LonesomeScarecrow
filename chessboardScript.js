e1246533a50e8602d0e20650c70979b2384dfeeb
BTzen <matthew_fb@live.ca>
2015-12-26 13:30:52 -0500

    piece.js and anton's updates from blocking commit

--- chessboardScript.js
+++ chessboardScript.js
@@ -15,11 +15,11 @@ function drawBoard(canvas, ctx) {
     for (var i = 0; i < 8; i++) {
         for (var j = 0; j < 8; j++) {
             if (!white) {
-                ctx.fillStyle = "rgb(245,222,179)";
+                ctx.fillStyle = "rgb(160,82,45)";
                 ctx.fillRect(j * 75, i * 75, 75, 75);
                 white = true;
             } else {
-                ctx.fillStyle = "rgb(160,82,45)";
+                ctx.fillStyle = "rgb(245,222,179)";
                 ctx.fillRect(j * 75, i * 75, 75, 75);
                 white = false;
             }
@@ -35,20 +35,49 @@ stored
 */
 function pushPieces(ctx) {
     chesspieces.push({
-        c: 9817,
+        unicode: 9817,
+        //colour can always be set to black
         colour: "rgb(0,0,0)",
+        //font can be hardcoded
         font: "70px Arial unicode MS",
         left: 0,
         top: 145,
+        //make width and height static, since each one takes up the square
+        width: 75,
+        height: 75,
+    });
+	
+	chesspieces.push({
+        unicode: 9817,
+        //colour can always be set to black
+        colour: "rgb(0,0,0)",
+        //font can be hardcoded
+        font: "70px Arial unicode MS",
+        left: 75,
+        top: 220,
+        //make width and height static, since each one takes up the square
+        width: 75,
+        height: 75,
+    });
+	
+	    chesspieces.push({
+        unicode: 9817,
+        //colour can always be set to black
+        colour: "rgb(0,0,0)",
+        //font can be hardcoded
+        font: "70px Arial unicode MS",
+        left: 0,
+        top: 295,
+        //make width and height static, since each one takes up the square
         width: 75,
         height: 75,
     });
 
     chesspieces.forEach(function(piece) {
-        var c = piece.c;
+        var unicode = piece.unicode;
         ctx.fillStyle = piece.colour;
         ctx.font = piece.font;
-        ctx.fillText(String.fromCharCode(c), piece.left, piece.top);
+        ctx.fillText(String.fromCharCode(unicode), piece.left, piece.top);
     });
 }
 
@@ -57,34 +86,60 @@ This method initializes on load and creates an onclick event.
 */
 function init() {
     canvas = document.getElementById("chessboard");
-    canvasGame = document.getElementById("topChessboard");
-    canvasLeft = canvas.offsetLeft,
-        canvasTop = canvas.offsetTop,
-        ctx = canvas.getContext('2d');
-    ctxGame = canvasGame.getContext('2d');
+    canvasPieces = document.getElementById("chesspieces");
+    canvasHighlight = document.getElementById("highlight");
+
+    canvasLeft = canvas.offsetLeft;
+    canvasTop = canvas.offsetTop;
+    ctx = canvas.getContext('2d');
+    ctxGame = canvasPieces.getContext('2d');
+	ctxHighlight = canvasHighlight.getContext('2d');
     drawBoard(canvas, ctx);
     pushPieces(ctxGame);
 
     //On click event will check what piece has been clicked
-    canvasGame.addEventListener('click', function(event) {
+    canvasPieces.addEventListener('click', function(event) {
         var x = event.pageX - canvasLeft,
             y = event.pageY - canvasTop;
         chesspieces.forEach(function(piece) {
             if (y > piece.top - 70 && y < piece.top - 70 + piece.height && x > piece.left && x < piece.left + piece.width) {
                 //-70 because text draws from the bottom and then up, unlike rectnagles which draw down and right
-                //alert(piece.c.toString()); //maybe change this to an id, but the unicode should tell you about their movement
+                alert(piece.unicode.toString()); //maybe change this to an id, but the unicode should tell you about their movement
                 if (!moveFlag) {
-                    ctxGame.fillStyle = "rgba(255, 255, 102, 0.5)";
-                    ctxGame.fillRect(piece.left, piece.top + 5, 75, 150);
-                    moveFlag = true;
+                    for (var i = 0; i < 2; i++) {
+                        if (checkTile(piece.left, piece.top +(i*75+5), ctxGame)) {
+                            ctxHighlight.fillStyle = "rgba(255, 255, 102, 0.5)";
+                            ctxHighlight.fillRect(piece.left, piece.top +(i*75+5), 75, 75);
+                            moveFlag = true;
+                        } else {
+                            break;
+                        }
+                    }
                 }
 
             } else {
-                ctxGame.clearRect(piece.left, piece.top + 5, 75, 150);
+                ctxHighlight.clearRect(piece.left, piece.top + 5, 75, 150);
                 moveFlag = false;
             }
         });
 
     }, false);
+}
+
+/**
+This will check an area of a 75x75 pixels to see if an object is there or not.
+if something is in that area return false
+*/
+function checkTile(x, y, context) {
+	//width and height = 75
+	var imgd = context.getImageData(x, y, 75,75);
+	var pix = imgd.data;
+	for (var i = 0, n = pix.length; i < n; i ++) {
+       var test = pix[i];
+	   if (test!=0) {
+		   return false;
+	   }
+    }
+    return true;
 }
 window.onload = init;
---
+++ piece.js
@@ -0,0 +1,105 @@
+//object constructor
+var Piece = function(type, isWhite, unicode, x, y) {
+	//warns the user if a Piece object is instantiated
+	if (this.__proto__ === Piece.prototype) {
+		alert("Piece is intended as an abstract class and should not be instantiated.");
+	}
+	// ************************************************************************ 
+	// PUBLIC PROPERTIES -- ANYONE MAY READ/WRITE 
+	// ************************************************************************
+	this.type = type;
+	this.isWhite = isWhite;
+	this.unicode = unicode;
+	this.x = x;
+	this.y = y;
+};
+
+//methods
+//valid if the desired location does not have a piece of the same color located there
+// Piece.prototype.isValidMove = function() {
+	// alert("isValidMove");
+// };
+
+//PAWN
+function Pawn(isWhite, x, y) {
+	//call superclass constructor.  Note that the constructor must pass the child object's context (ie. this)
+	Piece.call(this, "Pawn", isWhite, (isWhite) ? "&#9817;" : "&#9823;", x, y);
+	this.hasMoved = false;
+}
+
+//PAWN METHODS 
+
+//make Pawn inherit from Piece (might only be necessary if we want piece to have functions)
+Pawn.prototype = Object.create(Piece.prototype);	//creates empty object with given prototype
+Pawn.prototype.value = 1;	//make all chess pieces share same value (ie. what the piece is worth)
+
+Pawn.prototype.move = function(x, y) {
+	//move pawn
+	// if (Piece.isValidMove(newRow, newColumn) && ) {
+		// alert("");
+	// }	
+};
+
+Pawn.prototype.attack = function(x, y) {
+	
+};
+
+//KNIGHT
+
+function Knight(isWhite, x, y) {
+	Piece.call(this, "Knight", isWhite, (isWhite) ? "&#9816;" : "&#9822;", x, y);
+}
+
+Knight.prototype = Object.create(Piece.prototype);
+Knight.prototype.value = 3;
+
+//KNIGHT METHODS
+Knight.prototype.move = function() {};
+
+//BISHOP
+
+function Bishop(isWhite, x, y) {
+	Piece.call(this, "Bishop", isWhite, (isWhite) ? "&#9815;" : "&#9821;", x, y);
+}
+
+Bishop.prototype = Object.create(Piece.prototype);
+Bishop.prototype.value = 3;
+
+//BISHOP METHODS
+Bishop.prototype.move = function () {};
+
+//ROOK
+
+function Rook(isWhite, x, y) {
+	Piece.call(this, "Rook", isWhite, (isWhite) ? "&#9814;": "&#9820;", x, y);
+}
+
+Rook.prototype = Object.create(Piece.prototype);
+Rook.prototype.value = 5;
+
+//ROOK METHODS
+Rook.prototype.move = function () {};
+
+//QUEEN
+
+function Queen(isWhite, x, y) {
+	Piece.call(this, "Queen", isWhite, (isWhite) ? "&#9813;": "&#9819;", x, y);
+}
+
+Queen.prototype = Object.create(Piece.prototype);
+Queen.prototype.value = 9;
+
+//QUEEN METHODS
+Queen.prototype.move = function () {};
+
+//KING
+
+function King(isWhite, x, y) {
+	Piece.call(this, "King", isWhite, (isWhite) ? "&#9812;" : "&#9818;", x, y); 
+}
+
+King.prototype = Object.create(Piece.prototype);
+King.prototype.value = Number.MAX_VALUE;
+
+//KING METHODS
+King.prototype.move = function() {};
