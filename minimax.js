9ed94b69a30b9bdca1c89a462620ada29da59693
anton-kurilov <anton.kurilov@outlook.com>
2016-01-04 02:33:29 -0500

    night commit
    - countpieces is good and an eval function is starting to be created
    - next step to link to the ai

--- minimax.js
+++ minimax.js
@@ -2,83 +2,124 @@ var score;
 var nodeCount = -1;
 var currentScore = 0;
 
+//COUNTS THE NUMBER OF A PIECE ON THE BOARD
+function countPieces (board, pieceName, isWhite) {
+	var count = 0;
+	for (var i = 0; i < 8; i++) {
+		for (var j = 0; j < 8; j++) {
+			var toCheck = board.getPiece(i,j);
+			if (toCheck !== null) {
+				if (toCheck.type === pieceName && toCheck.isWhite == isWhite) {
+					count++;
+				}
+			}
+		}
+	}
+}
+
+//WEIGHT AND DIFFERENCE WILL BE SUMMED UP
+function evaluate(board) {
+	var whitePawns = countPieces(board, "Pawn", true);
+	var blackPawns = countPieces(board, "Pawn", false);
+	return whitePawns - blackPawns;
+}
+
+function maxi(alpha, beta, depth) {
+    if (depth === 0) {
+        currentScore = evaluate();
+        console.log("Depth: " + depth + " || Max: " + currentScore);
+        return currentScore;
+    }
+    //var max = Number.NEGATIVE_INFINITY;
+    for (var i = 0; i < 3; i++) { //set all moves = 3 for dummy tree
+        score = mini(alpha, beta, depth - 1);
+        if (score >= beta) {
+            return beta;
+        }
+        if (score > alpha) {
+            alpha = score;
+        }
+    }
+    console.log("Depth: " + depth + " || Max: " + alpha);
+    return alpha;
+}
+
+function mini(alpha, beta, depth) {
+    if (depth === 0) {
+        currentScore = evaluate();
+        console.log("Depth: " + depth + " || Min: " + currentScore);
+        return currentScore;
+    }
+    //var min = Number.POSITIVE_INFINITY;
+    for (var i = 0; i < 3; i++) {
+        score = maxi(alpha, beta, depth - 1);
+        if (score <= alpha) {
+            return alpha;
+        }
+        if (score < beta) {
+            beta = score;
+        }
+    }
+    console.log("Depth: " + depth + " || Min: " + beta);
+    return beta;
+}
+
+
+
+
+
+
+//Hardcoded example eval
+/*
 function evaluate() {
     nodeCount++;
-    switch(nodeCount) {
+    switch (nodeCount) {
         case 0:
             currentScore = 3;
-			return currentScore;
-			break;
+            return currentScore;
+            break;
         case 1:
             currentScore = 12;
-			return currentScore;
-			break;
+            return currentScore;
+            break;
         case 2:
             currentScore = 8;
-			return currentScore;
-			break;
+            return currentScore;
+            break;
         case 3:
             currentScore = 2;
-			return currentScore;
-			break;
+            return currentScore;
+            break;
+		/*
         case 4:
+		//these get pruned
             currentScore = 4;
-			return currentScore;
-			break;
-		case 5:
+            return currentScore;
+            break;
+        case 5:
             currentScore = 6;
-			return currentScore;
-			break;
-		case 6:
+            return currentScore;
+            break;
+		*//*
+        case 4:
             currentScore = 14;
-			return currentScore;
-			break;
-		case 7:
+            return currentScore;
+            break;
+        case 5:
             currentScore = 5;
-			return currentScore;
-			break;
-        case 8:
+            return currentScore;
+            break;
+        case 6:
             currentScore = 2;
-			return currentScore;
-			break;
-		default:
-			break;
+            return currentScore;
+            break;
+        default:
+            break;
     }
 
     return 1337;
 }
-
-function maxi(depth) {
-    if (depth === 0) {
-		currentScore = evaluate();
-        console.log("Depth: " + depth + " || Max: " + currentScore);
-        return currentScore;
-    }
-    var max = Number.NEGATIVE_INFINITY;
-    for (var i = 0; i < 3; i++) { //set all moves = 3 for dummy tree
-        score = mini(depth - 1);
-        if (score > max)
-            max = score;
-    }
-    console.log("Depth: " + depth + " || Max: " + max);
-    return max;
-}
-
-function mini(depth) {
-    if (depth === 0) {
-		currentScore = evaluate();
-        console.log("Depth: " + depth + " || Min: " + currentScore);
-        return currentScore;
-    }
-    var min = Number.POSITIVE_INFINITY;
-    for (var i = 0; i < 3; i++) {
-        score = maxi(depth - 1);
-        if (score < min)
-            min = score;
-    }
-    console.log("Depth: " + depth + " || Min: " + min);
-    return min;
-}
+*/
 
 
 /*
