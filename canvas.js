8219e35d06bc665de52408a43572891f5c773e35
Matt <BTzen@users.noreply.github.com>
2016-07-28 23:46:23 -0400

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,17 +1,11 @@
-/* if this method is not just checking the board it will display
- * the possible tiles a piece can move to, otherwise it will push the possible moves to the highlightedTiles
- * if isCheckingBoard is true the tiles will be added to an array rather than highlighted
+/* TODO consider getting rid of this method or moving it somewhere else
+ * if this method is not just checking the board it will display the possible tiles a piece can move to, otherwise it will push the possible moves to the highlightedTiles
  * bHighlightTiles boolean used to determine whether the tiles are visibly highlighted.  If set to false the potential moves are pushed to an array to be used for purposes of rule enforcement and board state evaluation
  */
- function fill(ctxHighlight, color, move) {//atActionType, row, column) {
-	if (move.column > -1 && move.column < 8 && move.row > -1 && move.row < 8) {
-		//
-		if (!isCheckingBoard) {
-			ctxHighlight.fillStyle = color;
-			ctxHighlight.fillRect(move.column * LENGTH, move.row * LENGTH, LENGTH, LENGTH);
-			highlightedTiles.push(move); 
-		} else {
-			allHighlightedTiles.push(move);	
-		}
+ function fill(ctxHighlight, color, action) {
+	if (action.column > -1 && action.column < 8 && action.row > -1 && action.row < 8) {
+		ctxHighlight.fillStyle = color;
+		ctxHighlight.fillRect(action.column * LENGTH, action.row * LENGTH, LENGTH, LENGTH);
+		highlightedTiles.push(action); 
 	}
 }
